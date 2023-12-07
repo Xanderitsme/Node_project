7713a1335ffbaf147f17285eba015e942ff41b10
xanderitsme <xanderitsme@gmail.com>
2023-12-07 16:34:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(3000, (err) => {
+    if (err) console.log("an error ocurred")
+    console.log("server running on port 3000");
+})
